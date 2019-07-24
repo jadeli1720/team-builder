a371eb19673102c0170204d6c9c60f062aab1fd7
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-07-24 16:00:59 -0600

    Created a TeamList array to try and pass my members array to the TeamList then create and map over it in that file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 import SignupForm from './components/Form';
+import TeamList from './components/TeamList';
 import { member } from './TeamData';
 
 import './App.css';
@@ -9,26 +10,25 @@ import './App.css';
 
 function App() {
 
-  const {teamMember, setTeamMember} = useState(member)
-  // console.log(member)
+  const {teamMember, setTeamMember} = useState([])
+  console.log(member)
   //may need to change the key and value. Value may become an empty string. 
   //? does the useState go outside or inside the App function?
     
-
   return (
     <div className="App">
       <h1>Who's on the team</h1>
       <h3>Add a Team Member:</h3>
-      <SignupForm />
-      <h3>Team Member List</h3> 
-      { console.log(member) }
-      {/* {teamMember.map((people, index) =>(
-        <div className="member-container">
-          <h4>Name: {people.member.name}</h4>
-          <p>Email: {people.member.email}</p>
-          <p>Role: {people.member.role}</p>
-        </div>
-      ))} */}
+      <SignupForm 
+          teamMember={teamMember}
+          setTeamMember={setTeamMember}
+      />
+
+      <TeamList
+          teamMember={teamMember}
+          setTeamMember={setTeamMember}
+      />
+      
         
     </div>
   );
---
+++ src/components/TeamList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function MemberList (props) {
+    const {members} = props
+    return (
+        <div>
+            {console.log('Members Array', members)}
+        </div>
+    )
+}
