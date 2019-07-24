240f070d349d0173884ae8504beec5f88ac81e52
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-07-24 13:27:09 -0600

    Added a state variable and gave it defualt value that will possibly keep track of team members.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+
+
 function App() {
+
+  const {teamMember, setTeamMember} = useState({key:"value"})
+  //may need to change the key and value. Value may become an empty string. 
+  //? does the useState go outside or inside the App function?
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
