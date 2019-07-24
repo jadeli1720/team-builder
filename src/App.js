40cfb09ca9c34dbae5e640672f78f161242c7e12
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-07-24 15:35:43 -0600

    Modified Form.js. The form will take in data and console log the typed in data properly. Need to figure out the map issue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 import SignupForm from './components/Form';
+import { member } from './TeamData';
 
 import './App.css';
 
@@ -8,37 +9,27 @@ import './App.css';
 
 function App() {
 
-  const {teamMember, setTeamMember} = useState({key:"value"})
+  const {teamMember, setTeamMember} = useState(member)
+  // console.log(member)
   //may need to change the key and value. Value may become an empty string. 
   //? does the useState go outside or inside the App function?
+    
 
   return (
     <div className="App">
       <h1>Who's on the team</h1>
       <h3>Add a Team Member:</h3>
       <SignupForm />
-      <h3>Team Member List</h3>
-      {/* Hard-coded example of what I need */}
-      <div>
-        <h4>Name: Jade</h4>
-        <p>Email: jade@gmail.com</p>
-        <p>Role: UX Designer</p>
-      </div>
-      <div>
-        <h4>Name: Landry</h4>
-        <p>Email: Landry@gmail.com</p>
-        <p>Role: Front End Developer</p>
-      </div>
-      <div>
-        <h4>Name: Adnan</h4>
-        <p>Email: Adnan@gmail.com</p>
-        <p>Role: Front End Framework Developer</p>
-      </div>
-      <div>
-        <h4>Name: Darien</h4>
-        <p>Email: Darien@gmail.com</p>
-        <p>Role: Back End  Developer</p>
-      </div>
+      <h3>Team Member List</h3> 
+      { console.log(member) }
+      {/* {teamMember.map((people, index) =>(
+        <div className="member-container">
+          <h4>Name: {people.member.name}</h4>
+          <p>Email: {people.member.email}</p>
+          <p>Role: {people.member.role}</p>
+        </div>
+      ))} */}
+        
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,63 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 
-export default function SignupForm() {
 
+export default function SignupForm(props) {
+    const [newPerson, setNewPerson] = useState({name:"", email:"", role:""})
+
+    function handleChange(event) {
+        const updatedUser ={...newPerson, [event.target.name]: event.target.value};
+        console.log(
+            "handleChange",
+            event.target.name,
+            event.target.value,
+            updatedUser
+        );
+        setNewPerson(updatedUser);
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log('name', newPerson);
+    }
 
     return (
-        <form>
-            
+        <form onSubmit={handleSubmit}>
+            <div className="form-group">
+                <label>Name:</label>
+                <input
+                    type="text"
+                    className="input"
+                    name="name"
+                    placeholder="Please enter the name"
+                    value={newPerson.name}
+                    onChange={handleChange}
+                />
+            </div>
+            <div className="form-group">
+                <label>Email:</label>
+                <input
+                    type="text"
+                    className="input"
+                    name="email"
+                    placeholder="Please enter a email"
+                    value={newPerson.email}
+                    onChange={handleChange}
+                />
+            </div>
+            <div className="form-group">
+                <label>Role:</label>
+                <input
+                    type="text"
+                    className="input"
+                    name="role"
+                    placeholder="Please enter their role"
+                    value={newPerson.role}
+                    onChange={handleChange}
+                />
+            </div>
+            <button type="submit" className="btn-form">
+          Submit</button>
         </form>
     )
 }
